ae4510116c40d6161601503ef1ab01819af8f419
Felipe Carmona <luis.carmonacastro@ucr.ac.cr>
2018-11-04 23:25:56 -0600

    Listones

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 from random import randint, choice
-
-j1 # Jugador 1
-j2 # Jugador 2 
-M # Matrix
+import csv
+import pandas as pd
+j1="" # Jugador 1
+j2="" # Jugador 2 
+M=[] # Matrix
 
 def crearMatriz(): #Crea la matriz de 30x30 con igual cantidad de números pares e impares
-    cPares = 0
-    cImpares = 0
-    global M
-    M = []
-    for i in range(30):
-        M.append([]) #La convierte matriz
-        for j in range(30):
-            M[i].append(randint(0, 9)) #Genera un número aleatorio entre 0 y 9
-            if cPares == 450 and cImpares != 450: #Si ya hay 450 pares llena con impares
-                M[i][j] = choice([1,3,5,7,9]) # Random de Impares
-            elif cPares != 450 and cImpares == 450: #Si ya hay 450 pares llena con impares
-                M[i][j] = choice([0,2,4,6,8]) # Random de Pares            
-            n = M[i][j]
-            if n%2 == 0: #Revisa si es Par
-                cPares=cPares+1
-            elif n%2 != 0: #Revisa si es Impar
-                cImpares=cImpares+1
-
+    print("¿Desea cargar la partida anterior?(s/n)")
+    eleccion = input()
+    if eleccion == "n":
+        cPares = 0
+        cImpares = 0
+        global M
+        M = []
+        for i in range(30):
+            M.append([]) #La convierte matriz
+            for j in range(30):
+                M[i].append(randint(0, 9)) #Genera un número aleatorio entre 0 y 9
+                if cPares == 450 and cImpares != 450: #Si ya hay 450 pares llena con impares
+                    M[i][j] = choice([1,3,5,7,9]) # Random de Impares
+                elif cPares != 450 and cImpares == 450: #Si ya hay 450 pares llena con impares
+                    M[i][j] = choice([0,2,4,6,8]) # Random de Pares            
+                n = M[i][j]
+                if n%2 == 0: #Revisa si es Par
+                    cPares=cPares+1
+                elif n%2 != 0: #Revisa si es Impar
+                    cImpares=cImpares+1
+                    
+        with open('matriz.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(M)
+    elif eleccion == "s":
+        # global M
+        M = []
+        with open('matriz.csv', newline='') as f:
+            reader = csv.reader(f, delimiter = ',')
+            i = 0
+            for row in reader:
+                M.append([]) #La convierte matriz
+                for j in range(30):
+                    row[j]=int(row[j])
+                M[i] = row
+                i=i+1
+                
+                # do something here with `row`
 def jugadores(): # Talvez no haga falta en una fuinción
     global j1
     global j2
@@ -48,75 +69,87 @@ def jugada(jugador1, jugador2, Matriz):
     intentosJ2 = 0 # a los 3 termina el juego
     p1 = 0 #Puntaje jugador 1
     p2 = 0 #Puntaje jugador 2
-    
-    while intentosJ1 < 3 and intentosJ2 < 3: 
-        print("/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-")
-        print (j1 + " ingrese la posición x de la matriz: ")
-        x1 = int(input())
-        print (j1 + " ingrese la posición y de la matriz: ")
-        y1 = int(input())    
-        casilla1 = M[x1][y1]
-        if casilla1 == -1:
-            print("¡Esta casilla ya esta usada, pierde el turno!")
+    salir = 1
+    while intentosJ1 < 3 and intentosJ2 < 3 and salir==1:
+        print("¿Desea seguir jugando? (s/n)")
+        respuesta = input()
+        if respuesta=="s":
+            turnos += 1
+            print("/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-")
+            print (j1 + " ingrese la posición x de la matriz: ")
+            x1 = int(input())
+            print (j1 + " ingrese la posición y de la matriz: ")
+            y1 = int(input())    
+            casilla1 = M[x1][y1]
+            if casilla1 == -1:
+                print("¡Esta casilla ya esta usada, pierde el turno!")
+                print(" ")
+            else:
+                sc1 = str(casilla1) #El str de la casilla para imprimirlo
+                print(j1 + " Su casilla es: " + sc1)
+                print(" ")
+                if casilla1%2 == 0 : #Si es par
+                    p1 = p1+1
+                    print("¡Ganó un punto!")
+                    print(" ")
+                else: 
+                    p1 = p1-1
+                    intentosJ1 = intentosJ1+1
+                    if intentosJ1 == 3:
+                        break
+                    print("¡Perdió un punto, manc@!")
+                    print(" ")
+                M[x1][y1] = -1 # la marca como usada
+            fJ1 = str(3 - intentosJ1) #El str que dice cuántos intentos quedan
+            sp1 = str(p1) #El str del puntaje para imprimirlo
+            print(j1 + " su puntaje acutal es: " + sp1)
+            print("Le quedan " + fJ1 + " intento(s)...")
             print(" ")
+            print("/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-")
+            print (j2 + " ingrese la posición x de la matriz: ")
+            x2 = int(input())
+            print (j2 + " ingrese la posición y de la matriz: ")
+            y2 = int(input())
+            casilla2 = M[x2][y2]
+            if casilla2 == -1:
+                print("¡Esta casilla ya esta usada, pierde el turno!")
+                print(" ")
+            else:
+                sc2 = str(casilla2) #El str de la casilla para imprimirlo
+                print(j2 + " Su casilla es: " + sc2)
+                print(" ")
+            
+                if casilla2%2 != 0 : #Si es impar
+                    p2 = p2+1
+                    print("¡Ganó un punto!")
+                    print(" ")
+                else: 
+                    p2 = p2-1
+                    intentosJ2 = intentosJ2+1
+                    if intentosJ2 == 3:
+                        break
+                    print("¡Perdió un punto, manc@!")
+                    print(" ")
+                M[x2][y2] = -1 # la marca como usada
+            fJ2 = str(3 - intentosJ2) #El str que dice cuántos intentos quedan
+            sp2 = str(p2) #El str del puntaje para imprimirlo
+            print(j2 + " su puntaje acutal es: " + sp2)
+            print("Le quedan " + fJ2 + " intento(s)...")
+            print(" ")
+            if turnos%5 == 0:
+                with open('matriz.csv', 'w', newline='', encoding='utf-8') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerows(M)
         else:
-            sc1 = str(casilla1) #El str de la casilla para imprimirlo
-            print(j1 + " Su casilla es: " + sc1)
-            print(" ")
-            if casilla1%2 == 0 : #Si es par
-                p1 = p1+1
-                print("¡Ganó un punto!")
-                print(" ")
-            else: 
-                p1 = p1-1
-                intentosJ1 = intentosJ1+1
-                if intentosJ1 == 3:
-                    break
-                print("¡Perdió un punto, manc@!")
-                print(" ")
-            M[x1][y1] = -1 # la marca como usada
-        fJ1 = str(3 - intentosJ1) #El str que dice cuántos intentos quedan
-        sp1 = str(p1) #El str del puntaje para imprimirlo
-        print(j1 + " su puntaje acutal es: " + sp1)
-        print("Le quedan " + fJ1 + " intento(s)...")
-        print(" ")
-        print("/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-/*-")
-        print (j2 + " ingrese la posición x de la matriz: ")
-        x2 = int(input())
-        print (j2 + " ingrese la posición y de la matriz: ")
-        y2 = int(input())
-        casilla2 = M[x2][y2]
-        if casilla2 == -1:
-            print("¡Esta casilla ya esta usada, pierde el turno!")
-            print(" ")
-        else:
-            sc2 = str(casilla2) #El str de la casilla para imprimirlo
-            print(j2 + " Su casilla es: " + sc2)
-            print(" ")
-        
-            if casilla2%2 != 0 : #Si es impar
-                p2 = p2+1
-                print("¡Ganó un punto!")
-                print(" ")
-            else: 
-                p2 = p2-1
-                intentosJ2 = intentosJ2+1
-                if intentosJ2 == 3:
-                    break
-                print("¡Perdió un punto, manc@!")
-                print(" ")
-            M[x2][y2] = -1 # la marca como usada
-        fJ2 = str(3 - intentosJ2) #El str que dice cuántos intentos quedan
-        sp2 = str(p2) #El str del puntaje para imprimirlo
-        print(j2 + " su puntaje acutal es: " + sp2)
-        print("Le quedan " + fJ2 + " intento(s)...")
-        print(" ")
+            salir = 0
     #final del while
     
     if intentosJ1 < 3: # Gana el jugador 1
         print("¡¡FELICIDADES " + j1 +" GANÓ!!")
     elif intentosJ2 < 3: # Gana el jugador 2
         print("¡¡FELICIDADES " + j2 +" GANÓ!!")
+    elif salir == 0:
+        print("¡Hasta la próxima!")
 
 crearMatriz()
 jugadores()
