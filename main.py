54cc57b0a5ac9c79ee8df32b8feb7c59fa037998
Akkarim <luis.carvajalrojas@ucr.ac.cr>
2018-11-15 01:04:25 -0600

    Listo

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from random import randint, choice
 import csv
+
 j1="" # Jugador 1
 j2="" # Jugador 2 
 M=[] # Matrix
-eleccion = ""
+eleccion="" #Elección de guardado
 
 def crearMatriz(): #Crea la matriz de 30x30 con igual cantidad de números pares e impares
+    global eleccion 
     print("¿Desea cargar la partida anterior?(s/n)")
-    global eleccion = input()
+    eleccion= input()
     if eleccion == "n":
         cPares = 0
         cImpares = 0
@@ -25,8 +27,7 @@ def crearMatriz(): #Crea la matriz de 30x30 con igual cantidad de números pares
                 if n%2 == 0: #Revisa si es Par
                     cPares=cPares+1
                 elif n%2 != 0: #Revisa si es Impar
-                    cImpares=cImpares+1
-                    
+                    cImpares=cImpares+1               
         with open('matriz.csv', 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(M)
@@ -42,36 +43,45 @@ def crearMatriz(): #Crea la matriz de 30x30 con igual cantidad de números pares
                     row[j]=int(row[j])
                 M[i] = row
                 i=i+1
-    print (M)
-                
-def jugadores():
-    global j1
-    global j2
-    print("Ingrese el nombre del jugador 1: ")
-    j1 = input()
-    print(" ")
-    print("Ingrese el nombre del jugador 2: ")
-    j2 = input()
-    print(" ")
-    print(j1 + " va a jugar por los números impares.")
-    print(j2 + " va a jugar por los números pares.")  
-    
-def jugada(jugador1, jugador2, Matriz):
-    print(" ")
+ 
+def jugada(Matriz):
     global j1
     global j2
     global M
-    if eleccion == "S":
-        j1 
-        
-    j1 = jugador1
-    j2 = jugador2
-    M = Matriz
-    turnos = 0
+    global eleccion
+    print(eleccion)
     intentosJ1 = 0 # a los 3 termina el juego
     intentosJ2 = 0 # a los 3 termina el juego
+    M = Matriz
+    turnos = 0
     p1 = 0 #Puntaje jugador 1
     p2 = 0 #Puntaje jugador 2
+    if eleccion == "s":
+        datos=[] #Vector para guardar los datos  
+        with open('Datos.csv') as csvarchivo:
+            entrada = csv.reader(csvarchivo)
+            for reg in entrada:
+                datos.append(reg)
+        j1 = datos[0][0]
+        j2 = datos[1][0]
+        p1 = int(datos[0][1])
+        p2 = int(datos[1][1])
+        intentosJ1 = int(datos[0][2])
+        intentosJ1 = int(datos[1][2])
+        print("El jugador 1 era: " + j1 + " con " + str(p1) + " puntos y " + str(intentosJ1) + " intentos.")
+        print("El jugador 2 era: " + j2 + " con " + str(p2) + " puntos y " + str(intentosJ2) + " intentos.")
+    elif eleccion == "n":    
+        print("Ingrese el nombre del jugador 1: ")
+        j1 = input()
+        print(" ")
+        print("Ingrese el nombre del jugador 2: ")
+        j2 = input()
+        print(" ")
+        
+    print(j1 + " va a jugar por los números impares.")
+    print(j2 + " va a jugar por los números pares.") 
+    
+    print(" ") 
     salir = 1
     while intentosJ1 < 3 and intentosJ2 < 3 and  salir==1:
         turnos += 1
@@ -138,7 +148,7 @@ def jugada(jugador1, jugador2, Matriz):
         print(" ")
         sTurnos = str(turnos)
         print("Número de turno " + sTurnos)
-        if turnos%2 == 0:
+        if turnos%2 == 0:#**********-*-----------------------------------------
             print("¿Desea guardarla partida? (s/n)")
             respuesta1 = input()
             print("¿Desea seguir jugando? (s/n)")
@@ -147,6 +157,12 @@ def jugada(jugador1, jugador2, Matriz):
                 with open('matriz.csv', 'w', newline='', encoding='utf-8') as csvfile:
                     writer = csv.writer(csvfile)
                     writer.writerows(M)
+                datos = [(j1, p1, intentosJ1), (j2, p2, intentosJ2)]
+                csvsalida = open('Datos.csv', 'w', newline='') #Para guardar los datos del jugador
+                archivo = csv.writer(csvsalida)
+                archivo.writerows(datos)
+                del archivo
+                csvsalida.close()
             if respuesta2 =="n":
                 salir = 0
     #final del while
@@ -162,213 +178,17 @@ def jugada(jugador1, jugador2, Matriz):
         if p1 < p2:
             print("¡¡FELICIDADES " + j2 +" GANÓ!!")
             
+
+#datos=[] #Vector para guardar los datos  
+#with open('Datos.csv') as csvarchivo:
+#    entrada = csv.reader(csvarchivo)
+#    for reg in entrada:
+#        datos.append(reg)
+#
+#print(datos[0][1])
+
 crearMatriz()
-jugadores()
-jugada(j1, j2, M)
+#jugadores()
+jugada(M)
 
 
-
-
-
-
-
-
-
-#!/usr/bin/python--------------------------------------------------------------
-# 
-#from random import randint
-# 
-## este es el juego buscaminas desarrollado en python.
-## crear matriz, esta funcion no invoca ninguna otra funcion del programa
-#def matriz(filas,columnas,caracter=False):
-#    tablero = []
-#    for i in xrange(0,filas):
-#        v = [caracter]*columnas
-#        tablero.append(v)
-#    return tablero
-# 
-## poner minas aleatoriamente, esta funcion no invoca ninguna otra funcion del programa
-#def minas(filas,columnas,tablero,minaz):
-#    mi = 1
-#    while mi <= minaz:
-#        fil = randint(0,filas - 1)
-#        col = randint(0,columnas - 1)
-#        if not tablero[fil][col]:
-#            tablero[fil][col] = True
-#            mi += 1
-#    return tablero
-# 
-## se crea el tablero de acuerdo a las especificaciones del jugador esta funcion invoca
-## a la funcion matriz(es para crear una matriz dado el numero de filas y columnas)
-## y a la funcion minas(es para colocar las pistas. las pistas son los numeros que aparecen en el juego)
-#def tablero1():
-#    opcion = input("quiere un juego aleatorio (si = 1)(no = 0): ")
-#    if opcion :
-#        while True:
-#            print ("que dificultad desea?")
-#            print 
-#            print ("\t1. facil.")
-#            print ("\t2. medio.")
-#            print ("\t3. dificil.")
-#            print 
-#            opcion = raw_input("elija una opcion(f o facil)(m o medio)(d o dificil): ")
-#            if opcion.lower() == ("f" or "facil"):
-#                tablero = matriz(15,10)
-#                minas(15,10,tablero,75)
-#                return tablero,15,10
-#            elif opcion.lower() == ("m" or "medio"):
-#                tablero = matriz(25,20)
-#                minas(25,20,tablero,250)
-#                return tablero,25,20
-#            elif opcion.lower() == ("d" or "dificil"):
-#                tablero = matriz(35,30)
-#                minas(35,30,tablero,525)
-#                return tablero,35,30
-#            else :
-#                "debe escoger una de las opciones del menu."
-#    else :
-#        filas = input("ingresa el numero de filas que deseas: ")
-#        columnas = input("ingresa el numero de columnas que desea: ")
-#        while True:
-#            mina = input("ingresa el numero de minas que desea: ")
-#            if mina <= filas * columnas:
-#                break
-#            print "el numero de minas debe ser menor que %d."%(filas*columnas)
-#        tablero = matriz(filas,columnas)
-#        minas(filas,columnas,tablero,mina)
-#        return tablero,filas,columnas
-# 
-## poner las pistas que son los numeros que aparecen en el tablero
-## esta funcion solo hace uso de la funcion matriz
-#def numeros(tablero,filas,columnas):
-#    nueva = matriz(filas,columnas,".")
-#    for i in xrange(0,filas):
-#        for j in xrange(0,columnas):
-#            # calcular el numero de vecinos de la celula que se esta vicitando
-#            n = 0
-#            if i > 0 and j > 0 and tablero[i - 1][j - 1]:
-#                n += 1
-#            if j > 0 and tablero[i][j - 1]:
-#                n += 1
-#            if i < filas - 1 and j > 0 and tablero[i + 1][j - 1]:
-#                n += 1
-#            if i > 0 and tablero[i - 1][j]:
-#                n += 1
-#            if i < filas - 1 and tablero[i + 1][j]:
-#                n += 1
-#            if i > 0 and j < columnas - 1 and tablero[i - 1][j + 1]:
-#                n += 1
-#            if j < columnas - 1 and tablero[i][j + 1]:
-#                n += 1
-#            if i < filas - 1 and j < columnas - 1 and tablero[i + 1][j + 1]:
-#                n += 1
-#            if not tablero[i][j]:
-#                nueva[i][j] = n
-#            else :
-#                nueva[i][j] = True
-#    tablero = nueva
-#    return tablero
-# 
-## mostrar el tablero de juego, esta funcion no hace uso de ninguna otra funcion
-#def mostrar(tablero,filas,columnas,caracter):
-#    for i in xrange(0,filas):
-#        for j in xrange(0,columnas):
-#            if j != columnas - 1 :
-#                if type(tablero[i][j]) == (int or str):
-#                    print tablero[i][j],
-#                elif (type(tablero[i][j]) == bool) and (tablero[i][j]):
-#                    print "*",
-#                else :
-#    
-#                    print caracter,
-#            else :
-#                if type(tablero[i][j]) == (int or str):
-#                    print tablero[i][j]
-#                elif (type(tablero[i][j]) == bool) and (tablero[i][j]):
-#                    print "*"
-#                else:
-#            
-#                    print caracter
-# 
-## esta es la funcion que desapa el tablero
-#def destapar(filas,columnas,fila,columna,tablero,nuevo):
-#    nuevo[fila][columna] = tablero[fila][columna]
-#    if tablero[fila][columna] == 0:
-#        if fila > 0:
-#            if (not tablero[fila - 1][columna]) and (nuevo[fila - 1][columna] != 0):
-#                destapar(filas,columnas,fila - 1,columna,tablero,nuevo)
-#            else :
-#                nuevo[fila - 1][columna] = tablero[fila - 1][columna]
-#        if fila < filas - 1:
-#            if (not tablero[fila + 1][columna]) and (nuevo[fila + 1][columna] != 0):
-#                destapar(filas,columnas,fila + 1,columna,tablero,nuevo)
-#            else :
-#                nuevo[fila + 1][columna] = tablero[fila + 1][columna]
-#        if columna > 0:
-#            if (not tablero[fila][columna - 1]) and (nuevo[fila][columna - 1] != 0):
-#                destapar(filas,columnas,fila,columna - 1,tablero,nuevo)
-#            else :
-#                nuevo[fila][columna - 1] = tablero[fila][columna - 1]
-#        if columna < columnas - 1:
-#            if (not tablero[fila][columna + 1]) and (nuevo[fila][columna + 1] != 0):
-#                destapar(filas,columnas,fila,columna + 1,tablero,nuevo)
-#            else :
-#                nuevo[fila][columna + 1] = tablero[fila][columna + 1]
-# 
-##esta funcion toma la jugada del jugador, esta funcion no hace uso de ninguna otra funcion
-#def jugada(filas,columnas):
-#    print "para hacer su jugada debe especificar tanto la fila como la columna."
-#    while True:
-#        fila = input("ingrese la fila: ")
-#        columna = input("ingrese la columna: ")
-#        if (1 <= fila <= filas) and (1 <= columna <= columnas):
-#            break
-#        print "debe escoger una ficha que este dentro del rango de fila y columna."
-#    return fila,columna
-# 
-## esta es la funcion principal en la que corre el juego. esta funcion hace uso de las siguientes funciones
-## matriz : cre una matriz dado el numero de filas y de columnas
-## mostrar : esta funcion muestra cualquier matriz en forma de tablero
-## jugada : esta funcion toma la jugada que hace el jugador
-#def jugar(tablero,filas,columnas):
-#    nueva = matriz(filas,columnas,".")
-#    while True:
-#        mostrar(nueva,filas,columnas,".")
-#        fila,columna = jugada(filas,columnas)
-#        if type(tablero[fila - 1][columna - 1]) == int:
-#            nueva[fila - 1][columna - 1] = tablero[fila - 1][columna - 1]
-#            if tablero[fila - 1][columna - 1] == 0:
-#                destapar(filas,columnas,fila - 1,columna - 1,tablero,nueva)
-#        else :
-#            mostrar(tablero,filas,columnas," ")
-#            print "has perdido!!!"
-#            print "eso te pasa por destapar la ficha en la posicion (%d,%d)."%(fila,columna)
-#            break
-#        acabar = True
-#        for i in xrange(0,filas):
-#            for j in range(0,columnas):
-#                if nueva[i][j] == ".":
-#                    acabar = False
-#                    break
-#                if not acabar:
-#                    break
-#        if acabar:
-#            print "habeis ganado el juego."
-#            print "felicitaciones!!!"
-# 
-#print "este es el clasico juego buscaminas."
-#print "el juego consiste en descubrir todas las fichas que no tengan minas(las cuales estan representadas por \"*\""
-#print "los numeros que aparecen en la pantalla indica cuantas bomas aparecen al rededor."
-#print "cuando aparece el numero 0 es porque no hay ninguna bomba al rededor."
-#print "para seleccionar la ficha a destapar tendra que especificar la fila y la columna."
-#print "que empieze el juego."
-# 
-#jugar1 = True
-# 
-#while jugar1:
-# 
-#    tablero,filas,columnas = tablero1()
-#    tablero = numeros(tablero,filas,columnas)
-#    jugar(tablero,filas,columnas)
-# 
-#    jugar1 = input("desea jugar ora vez (si = 1)(no = 0)")
